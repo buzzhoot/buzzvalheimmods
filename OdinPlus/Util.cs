734753d04bc8179894103024c1b15200ef9f214a
Buzz <buzzhoot@hotmail.com>
2021-03-16 04:39:30 +0800

    add reflaction utils

--- OdinPlus/Util.cs
+++ OdinPlus/Util.cs
@@ -34,6 +34,45 @@ namespace OdinPlus
             }
             return result;
         }
+public static object InvokePrivate(object instance, string name, object[] args = null)
+        {
+            MethodInfo method = instance.GetType().GetMethod(name, BindingFlags.NonPublic | BindingFlags.Instance);
 
+            if (method == null)
+            {
+                DBG.blogWarning("Method " + name + " does not exist on type: " + instance.GetType());
+                return null;
+            }
+
+            return method.Invoke(instance, args);
+        }
+
+        public static T GetPrivateField<T>(object instance, string name)
+        {
+            FieldInfo var = instance.GetType().GetField(name, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (var == null)
+            {
+                DBG.blogWarning("Variable " + name + " does not exist on type: " + instance.GetType());
+                return default(T);
+            }
+
+            return (T)var.GetValue(instance);
+        }
+
+        public static void SetPrivateField(object instance, string name, object value)
+        {
+            FieldInfo var = instance.GetType().GetField(name, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (var == null)
+            {
+                DBG.blogWarning("Variable " + name + " does not exist on type: " + instance.GetType());
+                return;
+            }
+
+            var.SetValue(instance, value);
+        }
+    }
+}
     }
 }
