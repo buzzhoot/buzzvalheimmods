b9c39698241174ddcdfdbe7afa9aec0ff5bd5a0e
Buzz <buzzhoot@hotmail.com>
2021-03-16 00:19:14 +0800

    Dodge Shorcut added

---
+++ DodgeShortcut/DBG.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DodgeShortcut
+{
+    class DBG
+    {
+        #region Debug
+        public static void cprt(string s)
+        {
+            global::Console.instance.Print(s);
+        }
+        public static void InfoTL(string s)
+        {
+            Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, s, 0, null);
+        }
+        public static void InfoCT(string s)
+        {
+            Player.m_localPlayer.Message(MessageHud.MessageType.Center, s, 0, null);
+        }
+        public static void blogInfo(object o)
+        {
+            Plugin.logger.LogInfo(o);
+        }
+        public static void blogWarning(object o)
+        {
+            Plugin.logger.LogWarning(o);
+        }
+        #endregion Debug
+    }
+}
