9223e6ac084c2911c8f729f8561e26477edb65a9
Buzz <buzzhoot@hotmail.com>
2021-03-17 07:25:49 +0800

    Mono Trans and polish

--- OdinPlus/Plugin.cs
+++ OdinPlus/Plugin.cs
@@ -17,28 +17,14 @@ namespace OdinPlus
 	{
 		#region Config Var
 		//public static ConfigEntry<int> nexusID;
+		public static ManualLogSource logger;
 		public static ConfigEntry<KeyboardShortcut> KS_SecondInteractkey;
 		public static ConfigEntry<KeyboardShortcut> KS_debug;
 		public static ConfigEntry<string> CFG_ItemSellValue;
 		public static ConfigEntry<string> CFG_Pets;
 		#endregion
 
-		#region Plugin Var
-		public static ManualLogSource logger;
-		public static string LastCmd;
-		//prefab
 		public static GameObject OdinPlusRoot;
-		public static GameObject OdinPrefab;
-		private static GameObject OdinNPCParent;
-		private static GameObject OdinGuiRoot;
-		//trader
-		private static OdinTrader m_odinTrader;
-		//Pet
-		public static GameObject PrefabParent;
-
-		#endregion
-
-		#region Mono
 		private void Awake()
 		{
 			Plugin.logger = base.Logger;
@@ -48,18 +34,16 @@ namespace OdinPlus
 			KS_SecondInteractkey = base.Config.Bind<KeyboardShortcut>("1Hotkeys", "Second Interact key", new KeyboardShortcut(KeyCode.F));
 			KS_debug = base.Config.Bind<KeyboardShortcut>("1Hotkeys", "debug key", new KeyboardShortcut(KeyCode.F3));
 			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
-			init();
+
+			//notice:: init here
+			OdinPlusRoot = new GameObject("OdinPlus");
+			OdinPlus.Root.AddComponent<OdinPlus>();
+			DontDestroyOnLoad(OdinPlusRoot);
+			OdinScore.init();
+
+
 			DBG.blogInfo("OdinPlus Loadded");
 		}
-		public void Start()
-		{
-			OdinScore.init();
-		}
-		public void Update()
-		{
-
-		}
-		#endregion
 
 		#region patch		
 		#region Player and Console
@@ -68,17 +52,18 @@ namespace OdinPlus
 		{
 			private static void Postfix(Player __instance)
 			{
-				if (CheckPlayerNull())//|| m_odinTrader == null)
+				if (CheckPlayerNull())//|| OdinPlus.m_odinTrader == null)
 				{
 					return;
 				}
 				if (KS_SecondInteractkey.Value.IsDown() && __instance.GetHoverObject() != null)
 				{
-					if (__instance.GetHoverObject().transform.parent.name == m_odinTrader.name)
+					if (__instance.GetHoverName()=="odin")
 					{
-						m_odinTrader.SwitchSkill();	
+						OdinPlus.m_odinTrader.SwitchSkill();
+						return;
 					}
-					
+
 				}
 				#region debug
 				if (KS_debug.Value.IsUp())
@@ -98,7 +83,7 @@ namespace OdinPlus
 		{
 			private static void Prefix()
 			{
-				ProcessCommands(global::Console.instance.m_input.text);
+				OdinPlus.ProcessCommands(global::Console.instance.m_input.text);
 			}
 		}
 		#endregion
@@ -108,7 +93,7 @@ namespace OdinPlus
 		{
 			private static void Postfix()
 			{
-				//skillIcon = ObjectDB.instance.GetItemPrefab("HelmetOdin").GetComponent<ItemDrop>().m_itemData.m_shared.m_icons[0];
+				OdinPlus.initAssets();
 				Pet.initIndicator();
 				OdinSE.init();
 				OdinSE.Register();
@@ -147,12 +132,12 @@ namespace OdinPlus
 				//DBG.blogWarning("loading");
 				if (CheckPlayerNull()) { return; }
 				OdinScore.loadOdinData(Player.m_localPlayer.GetPlayerName());
-				if (OdinNPCParent == null)
+				if (OdinPlus.OdinNPCParent == null)
 				{
-					OdinNPCParent = new GameObject("OdinNPCParent");
-					OdinNPCParent.transform.SetParent(OdinPlusRoot.transform);
+					OdinPlus.OdinNPCParent = new GameObject("OdinPlus.OdinNPCParent");
+					OdinPlus.OdinNPCParent.transform.SetParent(OdinPlusRoot.transform);
 				}
-				initOdinPrefab();
+				OdinPlus.initNPCs();
 			}
 		}
 
@@ -161,16 +146,16 @@ namespace OdinPlus
 		[HarmonyPatch(typeof(Trader), "Start")]
 		private static class Patch_Trader_Start
 		{
-		private static void Prefix(Trader __instance)
-		{
-			var c =OdinMeads.MeadList[0].GetComponent<ItemDrop>();
-			__instance.m_items.Add(new Trader.TradeItem
-						{
-							m_prefab = c,
-							m_stack = 1,
-							m_price = 1
-						});
-		}
+			private static void Prefix(Trader __instance)
+			{
+				var c = OdinMeads.MeadList[0].GetComponent<ItemDrop>();
+				__instance.m_items.Add(new Trader.TradeItem
+				{
+					m_prefab = c,
+					m_stack = 1,
+					m_price = 1
+				});
+			}
 		}
 		private static class Patch_Raven_Awake
 		{
@@ -204,10 +189,10 @@ namespace OdinPlus
 			private static void Prefix()
 			{
 				Pet.Clear();
-				if (!(OdinNPCParent == null))
+				if (!(OdinPlus.OdinNPCParent == null))
 				{
-					m_odinTrader.RestTerrian();
-					Destroy(OdinNPCParent);
+					OdinPlus.m_odinTrader.RestTerrian();
+					Destroy(OdinPlus.OdinNPCParent);
 					return;
 				}
 				//Destroy(PrefabParent);
@@ -227,23 +212,6 @@ namespace OdinPlus
 		#endregion
 
 		#region Tool
-		public static void CheckConsole()
-		{
-
-			if (global::Console.instance != null && global::Console.instance.m_chatWindow.gameObject.activeInHierarchy)
-			{
-				string cmd = LastCmd;
-				if (Input.GetKeyDown(KeyCode.Return))
-				{
-					if (cmd.Length > 0)
-					{
-						ProcessCommands(cmd);
-						return;
-					}
-				}
-				LastCmd = global::Console.instance.m_input.text;
-			}
-		}
 		public static bool CheckPlayerNull(bool log = false)
 		{
 			if (Player.m_localPlayer == null)
@@ -254,118 +222,9 @@ namespace OdinPlus
 			}
 			return false;
 		}
-		public static void ProcessCommands(string inCommand)
-		{
-			if (inCommand.Length > 0)
-			{
-				if (inCommand.StartsWith(" "))
-				{
-					inCommand = inCommand.Remove(0, 1);
-				}
-				if (inCommand == "bzd")
-				{
-					DBG.blogWarning("SEX");
-				}
-				if (inCommand == "test")
-				{
-					Destroy(OdinPlusRoot);
-				}
-			}
-		}
-		#endregion
-
-		#region Feature
-		public static void init()
-		{
-			OdinPlusRoot = new GameObject("OdinPlus");
-			PrefabParent = new GameObject("OdinPlusPrefabs");
-			PrefabParent.SetActive(false);
-			PrefabParent.transform.SetParent(OdinPlusRoot.transform);
-			OdinNPCParent = new GameObject("OdinNPCs");
-			OdinNPCParent.SetActive(false);
-			OdinNPCParent.transform.SetParent(OdinPlusRoot.transform);
-			DontDestroyOnLoad(OdinPlusRoot);
-		}
-		public static void initOdinPrefab()
-		{
-			var podin = ZNetScene.instance.GetPrefab("odin");
-			var pfire = ZNetScene.instance.GetPrefab("fire_pit");
-			var pcaul = ZNetScene.instance.GetPrefab("piece_cauldron");
-			var odin = CopyChildren(podin);
-			odin.transform.SetParent(OdinNPCParent.transform);
-			var c = new GameObject("coll");
-			c.AddComponent<CapsuleCollider>();
-			c.transform.SetParent(odin.transform);
-			c.transform.localScale = new Vector3(1, 2, 1);
-			c.transform.localPosition = Vector3.up;
-			var fire = CopyChildren(pfire);
-			var caul = CopyChildren(pcaul);
-			fire.transform.SetParent(OdinNPCParent.transform);
-			caul.transform.SetParent(OdinNPCParent.transform);
-
-
-			odin.transform.localPosition = new Vector3(0f, 0, 0f);
-			fire.transform.localPosition = new Vector3(1.5f, 0, -0.5f);
-			caul.transform.localPosition = new Vector3(1.5f, 0, -0.5f);
-			Destroy(fire.transform.Find("PlayerBase").gameObject);
-			fire.transform.Find("_enabled_high").gameObject.SetActive(true);
-			caul.transform.Find("HaveFire").gameObject.SetActive(true);
-			m_odinTrader = odin.AddComponent<OdinTrader>();
-			caul.AddComponent<OdinStore>();
-			Plugin.OdinPrefab = odin;
-			OdinNPCParent.SetActive(true);
-			//SE.init();
-		}
-		private static void CreateOdinGui(Transform GuiBase)
-		{
-			if (OdinGuiRoot != null) { return; }
-			var bak = GuiBase;//.Find("Store").Find("border (1)");
-			DBG.cprt((bak == null).ToString());
-			OdinGuiRoot = new GameObject("OdinGui");
-			// OdinGuiRoot.transform.SetParent(OdinGuiRoot.transform.parent.parent);
-			//RectTransform rt = OdinGuiRoot.GetComponent<RectTransform>();
-			//remove
-			//Destroy(OdinGuiRoot.GetComponent<StoreGui>());
-			//add
-			//OdinGuiRoot.AddComponent<OdinStoreGUI>();
-			//OdinGuiRoot.transform.SetParent(GuiBase);
-			//Instantiate(bak, OdinGuiRoot.transform);
-		}
 
 		#endregion
 
-		#region Utilities
-		public static GameObject CopyChildren(GameObject prefab)
-		{
-			int cc = prefab.transform.childCount;
-			GameObject r = new GameObject(prefab.name);
-			for (int i = 0; i < cc; i++)
-			{
-				var o = prefab.transform.GetChild(i).gameObject;
-				var a = Instantiate(o, r.transform);
-				a.name = o.name;
-			}
-			return r;
-		}
-		public static void CopyComponent(Component original, GameObject destination)
-		{
-			System.Type type = original.GetType();
-			Component copy = destination.AddComponent(type);
-			// Copied fields can be restricted with BindingFlags
-			FieldInfo[] fields = type.GetFields();
-			PropertyInfo[] props = type.GetProperties();
-			foreach (FieldInfo field in fields)
-			{
-				field.SetValue(copy, field.GetValue(original));
-			}
-			//foreach(PropertyInfo p in props)
-			//{
-			//    props.SetValue(copy, props.GetValue(original));
-			//}
-			return;
-		}
-
-		#endregion
 	}
 
 }
