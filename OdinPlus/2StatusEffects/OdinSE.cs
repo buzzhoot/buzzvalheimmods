1a70d47b5ff4601360a0b4beb418b9a0166b5bca
Buzz <buzzhoot@hotmail.com>
2021-03-21 21:54:21 +0800

    Add name for se

--- OdinPlus/2StatusEffects/OdinSE.cs
+++ OdinPlus/2StatusEffects/OdinSE.cs
@@ -32,27 +32,29 @@ namespace OdinPlus
 			DBG.blogInfo("Register SE");
 		}
 
-		#region Buzz_SE
+		#region Pet_SE
 		private void initTrollSE()
 		{
 			var se = ScriptableObject.CreateInstance<SE_SumonPet>();
+			se.name = "SE_Troll";
 			se.m_icon = OdinPlus.TrollHeadIcon;
 			se.m_name = "$odin_se_troll";
 			se.m_tooltip = "$odin_se_troll_tooltip";
 			se.m_cooldownIcon = true;
 			se.m_ttl = 300;
-			se.PetName="TrollPet";
+			se.PetName = "TrollPet";
 			SElist.Add("scroll_troll", se);
 		}
 		private void initWolfSE()
 		{
 			var se = ScriptableObject.CreateInstance<SE_SumonPet>();
+			se.name = "SE_Wolf";
 			se.m_icon = OdinPlus.WolfHeadIcon;
 			se.m_name = "$odin_se_wolf";
 			se.m_tooltip = "$odin_se_wolf_tooltip";
 			se.m_cooldownIcon = true;
 			se.m_ttl = 300;
-			se.PetName="WolfPet";
+			se.PetName = "WolfPet";
 			SElist.Add("scroll_wolf", se);
 		}
 
@@ -69,15 +71,16 @@ namespace OdinPlus
 		private void CreateValSE(string name)
 		{
 			var se = ScriptableObject.CreateInstance<SE_Stats>();
+			se.name = name;
 			se.m_icon = OdinPlus.OdinSEIcon[0];
 			se.m_name = "$odin_se_" + name;
 			se.m_tooltip = "$odin_" + name + "_tooltip";
-			
-			se.m_ttl=5;
 
-			se.m_raiseSkill=Skills.SkillType.All;
-			se.m_raiseSkillModifier=100;
-			
+			se.m_ttl = 5;
+
+			se.m_raiseSkill = Skills.SkillType.All;
+			se.m_raiseSkillModifier = 100;
+
 			SElist.Add(name, se);
 		}
 		#endregion Val_SE
