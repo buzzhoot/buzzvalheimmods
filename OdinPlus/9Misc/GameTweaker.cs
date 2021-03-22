d3eed9901808905facdcb8d95605c5f088e6c98f
Buzz <buzzhoot@hotmail.com>
2021-03-23 02:58:02 +0800

    Add Raven

--- OdinPlus/9Misc/GameTweaker.cs
+++ OdinPlus/9Misc/GameTweaker.cs
@@ -9,5 +9,23 @@ namespace OdinPlus
 			humanoid.m_speed = speed;
 			return humanoid;
 		}
+	public static Tutorial.TutorialText SendRavenMessage(string messageName, string messageText)
+		{
+			Tutorial.TutorialText tutorialText = new Tutorial.TutorialText
+			{
+				m_label = "OdinQuest",
+				m_name = messageName,
+				m_text = messageText,
+				m_topic = "Quest Hints"
+			};
+			if (!Tutorial.instance.m_texts.Contains(tutorialText))
+			{
+				Tutorial.instance.m_texts.Add(tutorialText);
+			}
+			Player.m_localPlayer.ShowTutorial(tutorialText.m_name, true);
+			return tutorialText;
+		}
+	
 	}
+	
 }
