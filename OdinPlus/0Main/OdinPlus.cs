936577b6c90a551d00102bddf2815d4bb14b47d3
Buzz <buzzhoot@hotmail.com>
2021-03-23 03:02:43 +0800

    Add TaskManger

--- OdinPlus/0Main/OdinPlus.cs
+++ OdinPlus/0Main/OdinPlus.cs
@@ -63,6 +63,7 @@ namespace OdinPlus
 			Root.AddComponent<OdinMeads>();
 			Root.AddComponent<OdinItem>();
 			Root.AddComponent<PetManager>();
+			Root.AddComponent<TaskManager>();
 			isInit = true;
 		}
 		public static void PostODB()
