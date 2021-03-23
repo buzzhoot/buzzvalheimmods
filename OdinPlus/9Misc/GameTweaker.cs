9f54c30a0ec6286db1baa3293e11557982e8bd1e
Buzz <buzzhoot@hotmail.com>
2021-03-23 23:53:34 +0800

    Add method for depack v2i

--- OdinPlus/9Misc/GameTweaker.cs
+++ OdinPlus/9Misc/GameTweaker.cs
@@ -22,7 +22,7 @@ namespace OdinPlus
 			{
 				Tutorial.instance.m_texts.Add(tutorialText);
 			}
-			Player.m_localPlayer.ShowTutorial(tutorialText.m_name, false);
+			Player.m_localPlayer.ShowTutorial(tutorialText.m_name, true);
 			return tutorialText;
 		}
 		public static string GetTransName(this string str)
@@ -32,6 +32,17 @@ namespace OdinPlus
 		public static string GetLocal(this string str)
 		{
 			return Localization.instance.Localize(str);
+		}
+		public static string DepakVector2i(this string str, Vector2i v2i)
+		{
+			return v2i.x.ToString() + "_" + v2i.y.ToString();
+		}
+		public static Vector2i Pak(this Vector2i val, string str)
+		{
+			string[] a = str.Split(new char[] { '_' });
+			val.x = int.Parse(a[0]);
+			val.y = int.Parse(a[1]);
+			return val;
 		}
 	}
 
