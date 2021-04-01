39942652f3d9a63c7857c55b52fa2f35f3d4b6d7
BuzzHoot <buzzhoot@hotmail.com>
2021-04-02 06:10:58 +0800

    init plugin.cs

--- BuzzValheimTemplate/Plugin.cs
+++ BuzzValheimTemplate/Plugin.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using BepInEx.Logging;
+using UnityEngine;
+
 
 namespace BuzzValheimTemplate
 {
-    public class Plugin
-    {
+	[BepInPlugin("buzz.valhei.name", "name", "0.0.1")]
+	public class Plugin:BaseUnityPlugin
+	{
+        #region Var
+            
+            //public static ConfigEntry<int> nexusID;
+             private static ManualLogSource logger;
+        #endregion
+       
+		private void Awake()
+		{
+		Plugin.logger = base.Logger;
+        
+        //Plugin.nexusID = base.Config.Bind<int>("General", "NexusID", 354, "Nexus mod ID for updates");
+        Plugin.logger.LogInfo("name Loadded"); 
+		}
+
     }
 }
