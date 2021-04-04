54484c24eb648026f83b2a1f01d26e60e15b50e7
BuzzHoot <buzzhoot@hotmail.com>
2021-04-04 23:20:55 +0800

    fix bug

--- OdinPlus/1NPC/OdinTrader.cs
+++ OdinPlus/1NPC/OdinTrader.cs
@@ -12,14 +12,6 @@ namespace OdinPlus
 
 		#endregion,
 		#region Mono
-		private void Awake()
-		{
-			//var td = ZNetScene.instance.GetPrefab("Haldor").GetComponent<Trader>();
-			//m_talker = this.gameObject;
-			//m_randomStartTradeFX = td.m_randomStartTradeFX;
-		}
-		//private new void Start() { }
-		//private new void Update() { }
 		#endregion
 		#region Features
 
@@ -43,8 +35,6 @@ namespace OdinPlus
 			}
 			Say("$op_pot_open");
 			StoreGui.instance.Show(this);
-			//m_randomStartTradeFX.Create(m_talker.transform.position, Quaternion.identity, null, 1f);
-			//m_randomStartTradeFX.Create(this.transform.position, Quaternion.identity, null, 1f);
 			return true;
 		}
 		public new bool UseItem(Humanoid user, ItemDrop.ItemData item)
@@ -53,7 +43,7 @@ namespace OdinPlus
 		}
 		#endregion
 		#region Tool
-		private void Say(string text)
+		protected void Say(string text)
 		{
 			Chat.instance.SetNpcText(m_talker, Vector3.up * 1.5f, 60f, 5, m_talker.GetComponent<OdinNPC>().m_name, text, false);
 		}
