b49660f545d55779d071836f0b86e571f8996bc7
BuzzHoot <buzzhoot@hotmail.com>
2021-04-04 11:13:10 +0800

    questref done

--- OdinPlus/5Quest/QuestRef.cs
+++ OdinPlus/5Quest/QuestRef.cs
@@ -1,10 +1,43 @@
+using System.Collections.Generic;
 using System;
 
 namespace OdinPlus
 {
-    public class QuestRef
-    {
-    }
+	public class QuestRef
+	{
+		public static List<string[]> DungeonLoc = new List<string[]>{
+		new string[]{ "Crypt3" },
+		 new string[]{ "Crypt3", "Crypt2", "Crypt4" },
+		 new string[]{ "SunkenCrypt4" },
+		 new string[]{ "SunkenCrypt4" },
+		 new string[]{ "GoblinCamp2" },
+		 new string[]{ "Crypt3", "Crypt2", "Crypt4", "SunkenCrypt4", "GoblinCamp2" }};
+		public static List<string[]> TreasureLoc = new List<string[]> {
+				new string[] { "WoodHouse11", "WoodHouse6", "WoodHouse3", "WoodHouse4", "WoodHouse6", "WoodHouse7", "WoodHouse8", "WoodHouse9" },
+				new string[] { "WoodHouse3", "WoodHouse4", "Ruin2", "Ruins1", "ShipSetting01", "Runestone_Boars", "Runestone_Meadows", "Runestone_Greydwarfs", "Runestone_BlackForest" },
+				new string[] { "SwampRuin1", "SwampRuin2", "SwampHut5", "SwampHut1", "SwampHut2", "SwampHut3", "SwampHut4", "Runestone_Draugr", "FireHole", "DrakeNest01", "Waymarker02", "AbandonedLogCabin02", "AbandonedLogCabin03", "AbandonedLogCabin04", "MountainGrave01" },
+				new string[] { "DrakeNest01", "Waymarker02", "AbandonedLogCabin02", "AbandonedLogCabin03", "AbandonedLogCabin04", "MountainGrave01", "DrakeLorestone" },
+				new string[] { "StoneHenge1", "StoneHenge2", "StoneHenge3", "StoneHenge4", "StoneHenge5", "StoneHenge6" },
+				new string[] {"WoodHouse11","WoodHouse6","WoodHouse3","WoodHouse4","WoodHouse6","WoodHouse7","WoodHouse8","WoodHouse9","WoodHouse3",
+				"WoodHouse4","Ruin2","Ruins1","ShipSetting01","Runestone_Boars","Runestone_Meadows","Runestone_Greydwarfs","Runestone_BlackForest","SwampRuin1",
+				"SwampRuin2","SwampHut5","SwampHut1","SwampHut2","SwampHut3","SwampHut4","Runestone_Draugr","FireHole","DrakeNest01","Waymarker02",
+				"AbandonedLogCabin02","AbandonedLogCabin03","AbandonedLogCabin04","MountainGrave01","DrakeNest01","Waymarker02","AbandonedLogCabin02",
+				"AbandonedLogCabin03","AbandonedLogCabin04","MountainGrave01","DrakeLorestone","StoneHenge1","StoneHenge2","StoneHenge3","StoneHenge4","StoneHenge5","StoneHenge6"}};
+		public List<string[]> HuntLoc = new List<string[]>{
+			new string[] { "Runestone_Greydwarfs" },
+			new string[] { "Ruin1", "Runestone_Greydwarfs" },
+			new string[] { "Runestone_Draugr" },
+			new string[] { "Waymarker02" },
+			new string[] { "Runestone_Plains" },
+			new string[] { "Runestone_Greydwarfs", "Ruin1", "Runestone_Greydwarfs", "Runestone_Draugr", "Waymarker02", "Runestone_Plains" }};
+		public static string[] HunterMonsterList = new string[] { "Troll", "Draugr_Elite", "Fenring", "GoblinBrute" };
+		public static List<string[]> SearchItem = new List<string[]> {
+			new string[] { "LeatherScraps:20", "Mushroom:20", "CookedMeat:20", "Raspberry:20", "Stone:50", "Wood:50", "DeerHide:15", "Resin:20" },
+			new string[] { "TrollHide:10", "Coins:100", "Resin:50", "BoneFragments:20", "MushroomYellow:20", "Blueberries:20", "HardAntler:1" },
+			new string[] { "Guck:30", "Ooze:10", "SurtlingCore:20", "ElderBark:50", "Amber:20", "AmberPearl:20", "Resin:100" },
+			new string[] { "Guck:50", "Ooze:20", "SurtlingCore:30", "ElderBark:50", "DragonEgg:1", "WolfFang:20", "WolfPelt:10", "Resin:100" },
+			new string[] { "Amber:20", "AmberPearl:20", "Bread:50", "Guck:80", "Ooze:40", "SurtlingCore:30", "ElderBark:80", "DragonEgg:1", "WolfFang:20", "WolfPelt:20", "Resin:100" }};
+	}
 }
 
 
