21f622cc664778ad822b024ca4c465a74026122e
Buzz <buzzhoot@hotmail.com>
2021-04-05 11:04:41 +0800

    add init

--- OdinPlus/5Quest/QuestProcesser.cs
+++ OdinPlus/5Quest/QuestProcesser.cs
@@ -20,9 +20,29 @@ namespace OdinPlus
 		public QuestProcesser() { }
 		public virtual void Init()
 		{
-
+			var list1 = QuestRef.LocDic[quest.GetQuestType()];
+			var list2 = list1[quest.Key];
+			quest.locName = list2.GetRandomElement();
+			if (quest.GetQuestType() == QuestType.Search)
+			{
+				Begin();
+				return;
+			}
+			ZRoutedRpc.instance.InvokeRoutedRPC("RPC_ServerFindLocation", new object[] { quest.locName, quest.m_realPostion });
+			QuestManager.instance.Invoke("ShowWaitError", 10);
 		}
-		public virtual void Begin() { }
+		public virtual void Begin()
+		{
+			QuestManager.instance.MyQuests.Add(quest.ID, quest);
+			quest.SetMuninHints();
+			quest.SetMuninMessage();
+			quest.m_owenerName = Player.m_localPlayer.GetPlayerName();
+			quest.Begin();
+		}
+		public virtual void Place(LocationMarker lm)
+		{
+			lm.Used();
+		}
 
 	}
 }
