ab7c80e72ffa0188414bb0b9f16f09f2a1d6944b
Buzz <buzzhoot@hotmail.com>
2021-04-07 04:55:07 +0800

    placing method

--- OdinPlus/5Quest/TreasureQuestProcesser.cs
+++ OdinPlus/5Quest/TreasureQuestProcesser.cs
@@ -4,6 +4,23 @@ namespace OdinPlus
 {
 	public class TreasureQuestProcesser : QuestProcesser
 	{
-		
+		public override void Place(LocationMarker lm)
+		{
+			var pos = lm.GetPosition();
+			float y = -2f;
+			float x = 4f;
+			float z = 3.999f;
+			if (quest.Key == 0)
+			{
+				y = 0;
+				x = 2f;
+				z = 1.999f;
+			}
+			pos +=new Vector3(x.RollDice(),y,z.RollDice());
+			//add placing for no physics
+			var chest=LegacyChest.Place(pos,quest.ID,quest.Key);
+			DBG.blogWarning("Sever Placed LegacyChest at : " + pos);
+			base.Place(lm);
+		}
 	}
 }
